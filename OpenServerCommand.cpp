4758df5acf5aaa7d27ab7d86194eb8007915f969
danielsaraf94 <osychod@gmail.com>
2019-12-19 18:33:12 +0200

    daniels

--- OpenServerCommand.cpp
+++ OpenServerCommand.cpp
@@ -52,29 +52,41 @@ void OpenServerCommand::execute(string *str) {
 
 void OpenServerCommand::initialSimToNumMap() {
   this->numTosim[0] = "/instrumentation/airspeed-indicator/indicated-speed-kt";
-  this->numTosim[1] = "//instrumentation/heading-indicator/offset-deg";
-  this->numTosim[2] = "/instrumentation/altimeter/indicated-altitude-ft";
-  this->numTosim[3] = "/instrumentation/altimeter/pressure-alt-ft";
-  this->numTosim[4] = "/instrumentation/attitude-indicator/indicated-pitch-deg";
-  this->numTosim[5] = "/instrumentation/attitude-indicator/indicated-roll-deg";
-  this->numTosim[6] = "/instrumentation/attitude-indicator/internal-pitch-deg";
-  this->numTosim[7] = "/instrumentation/attitude-indicator/internal-roll-deg";
-  this->numTosim[8] = "/instrumentation/encoder/indicated-altitude-ft";
-  this->numTosim[9] = "/instrumentation/encoder/pressure-alt-ft";
-  this->numTosim[10] = "/instrumentation/gps/indicated-altitude-ft";
-  this->numTosim[11] = "/instrumentation/gps/indicated-ground-speed-kt";
-  this->numTosim[12] = "/instrumentation/gps/indicated-vertical-speed";
-  this->numTosim[13] = "/instrumentation/heading-indicator/indicated-heading-deg";
-  this->numTosim[14] = "/instrumentation/magnetic-compass/indicated-heading-deg";
-  this->numTosim[15] = "/instrumentation/slip-skid-ball/indicated-slip-skid";
-  this->numTosim[16] = "/instrumentation/turn-indicator/indicated-turn-rate";
-  this->numTosim[17] = "/instrumentation/vertical-speed-indicator/indicated-speed-fpm";
-  this->numTosim[18] = "/controls/flight/aileron";
-  this->numTosim[19] = "/controls/flight/elevator";
-  this->numTosim[20] = "/controls/flight/rudder";
-  this->numTosim[21] = "/controls/flight/flaps";
-  this->numTosim[22] = "/controls/engines/engine/throttle";
-  this->numTosim[23] = "/engines/engine/rpm";
+  this->numTosim[1] = "/sim/time/warp";
+  this->numTosim[2] = "/controls/switches/magnetos";
+  this->numTosim[3] = "/instrumentation/heading-indicator/offset-deg";
+  this->numTosim[4] = "/instrumentation/altimeter/indicated-altitude-ft";
+  this->numTosim[5] = "/instrumentation/altimeter/pressure-alt-ft";
+  this->numTosim[6] = "/instrumentation/attitude-indicator/indicated-pitch-deg";
+  this->numTosim[7] = "/instrumentation/attitude-indicator/indicated-roll-deg";
+  this->numTosim[8] = "/instrumentation/attitude-indicator/internal-pitch-deg";
+  this->numTosim[9] = "/instrumentation/attitude-indicator/internal-roll-deg";
+  this->numTosim[10] = "/instrumentation/encoder/indicated-altitude-ft";
+  this->numTosim[11] = "/instrumentation/encoder/pressure-alt-ft";
+  this->numTosim[12] = "/instrumentation/gps/indicated-altitude-ft";
+  this->numTosim[13] = "/instrumentation/gps/indicated-ground-speed-kt";
+  this->numTosim[14] = "/instrumentation/gps/indicated-vertical-speed";
+  this->numTosim[15] = "/instrumentation/heading-indicator/indicated-heading-deg";
+  this->numTosim[16] = "/instrumentation/magnetic-compass/indicated-heading-deg";
+  this->numTosim[17] = "/instrumentation/slip-skid-ball/indicated-slip-skid";
+  this->numTosim[18] = "/instrumentation/turn-indicator/indicated-turn-rate";
+  this->numTosim[19] = "/instrumentation/vertical-speed-indicator/indicated-speed-fpm";
+  this->numTosim[20] = "/controls/flight/aileron";
+  this->numTosim[21] = "/controls/flight/elevator";
+  this->numTosim[22] = "/controls/flight/rudder";
+  this->numTosim[23] = "/controls/flight/flaps";
+  this->numTosim[24] = "/controls/engines/engine/throttle";
+  this->numTosim[25] = "/controls/engines/current-engine/throttle";
+  this->numTosim[26] = "/controls/switches/master-avionics";
+  this->numTosim[27] = "/controls/switches/starter";
+  this->numTosim[28] = "/engines/active-engine/auto-start";
+  this->numTosim[29] = "/controls/flight/speedbrake";
+  this->numTosim[30] = "/sim/model/c172p/brake-parking";
+  this->numTosim[31] = "/controls/engines/engine/primer";
+  this->numTosim[32] = "/controls/engines/current-engine/mixture";
+  this->numTosim[33] = "/controls/switches/master-bat";
+  this->numTosim[34] = "/controls/switches/master-alt";
+  this->numTosim[35] = "/engines/engine/rpm";
 }
 
 void OpenServerCommand::readFromClient(int client_socket,
