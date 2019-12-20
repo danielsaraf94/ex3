7d9076ed327825c81d3ca924a698bd06430db314
shlomo4945 <shlomo4945@gmail.com>
2019-12-20 15:12:48 +0200

    updates

--- ConditionParser.cpp
+++ ConditionParser.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "ConditionParser.h"
+ConditionParser::ConditionParser(unordered_map<string, Command *> *map1, unordered_map<string, Data *>* map2) {
+  this->command_map = map1;
+  this->symbol_table=map2;
+}
+
+int ConditionParser::exectue(vector<string> *string_vec, int i) {
+  int return_index = returnIndex(string_vec, i);
+  if (isTrue((*string_vec)[i])) {
+    while (i != return_index - 1) {
+      Command* c = (*command_map)[(*string_vec)[i]];
+      i+=c->execute(string_vec,i+1);
+    }
+  }
+  return return_index;
+}
+
+int ConditionParser::returnIndex(vector<string> *string_vec, int i) {
+  while ((*string_vec)[i].find("{") == -1) {
+    i++;
+  }
+  this->queue.push("{");
+  while (this->queue.empty() == false) {
+    if ((*string_vec)[i].find("{") != -1) {
+      this->queue.push("{");
+    }
+    if ((*string_vec)[i].find("}") != -1) {
+      this->queue.pop();
+    }
+    i++;
+  }
+  return i;
+}
--- ConditionParser.h
+++ ConditionParser.h
@@ -6,10 +6,19 @@
 #define EX3_1_CMAKE_BUILD_DEBUG_CONDITIONPARSER_H_
 
 #include <Command.h>
+#include <unordered_map>
+#include "list"
+#include "queue"
+#include "Data.h"
 class ConditionParser: public Command {
+  unordered_map<string, Data *> *symbol_table;
+  unordered_map<string, Command *> *command_map;
+  queue<string> queue;
  public:
-  bool isTrue();
+  ConditionParser(unordered_map<string, Command *>*,unordered_map<string, Data *>*);
+  bool isTrue(string);
   int exectue(vector<string>*,int i);
+  int returnIndex(vector<string>*,int);
 };
 
 #endif //EX3_1_CMAKE_BUILD_DEBUG_CONDITIONPARSER_H_
